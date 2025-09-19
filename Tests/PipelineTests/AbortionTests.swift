950bec51db75de4e5602f1031b894d655c41f0c8
stefanspringer1 <stefan.springer@icloud.com>
2025-09-19 18:00:51 +0200

    Update AbortionTests.swift

--- Tests/PipelineTests/AbortionTests.swift
+++ Tests/PipelineTests/AbortionTests.swift
@@ -22,7 +22,7 @@ import Foundation
         func step2a<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
             execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
                 step3aa(during: execution)
-                execution.log(.fatal, "cannot proceess any further") // !!!! fatal error is here !!!!
+                execution.log(.fatal, "cannot proceess the item any further") // !!!! fatal error is here !!!!
                 step3ab(during: execution)
             }
         }
@@ -63,7 +63,69 @@ import Foundation
                 {progress} beginning step step2a(during:abort:)@PipelineTests
                     {progress} beginning step step3aa(during:abort:)@PipelineTests
                     {progress} ending step step3aa(during:abort:)@PipelineTests
-                    {fatal} cannot proceess any further
+                    {fatal} cannot proceess the item any further
+                    {progress} skipping in an aborted environment step step3ab(during:abort:)@PipelineTests
+                {progress} ending step step2a(during:abort:)@PipelineTests
+                {progress} skipping in an aborted environment step step2b(during:abort:)@PipelineTests
+            {progress} ending step step1(during:)@PipelineTests
+            """)
+        
+    }
+    
+    @Test func testDeadlyError() throws {
+        
+        func step1<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>) {
+            execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
+                step2a(during: execution)
+                step2b(during: execution)
+            }
+        }
+        
+        func step2a<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
+            execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
+                step3aa(during: execution)
+                execution.log(.deadly, "cannot proceess anything any more") // !!!! deadly error is here !!!!
+                step3ab(during: execution)
+            }
+        }
+        
+        func step2b<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
+            execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
+                step3b(during: execution)
+            }
+        }
+        
+        func step3aa<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
+            execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
+                // -
+            }
+        }
+        
+        func step3ab<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
+            execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
+                // -
+            }
+        }
+        
+        func step3b<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
+            execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
+                // -
+            }
+        }
+        
+        let logger = CollectingLogger()
+        let myExecutionInfoConsumer = ExecutionInfoConsumerForLogger<MyMetaData1>(logger: logger, excutionInfoFormat: ExecutionInfoFormat(withIndentation: true, withType: true))
+        
+        let execution = Execution<MyMetaData1>(metadata: metadata, executionInfoConsumer: myExecutionInfoConsumer)
+        
+        step1(during: execution)
+        
+        #expect(logger.messages.joined(separator: "\n") == """
+            {progress} beginning step step1(during:)@PipelineTests
+                {progress} beginning step step2a(during:abort:)@PipelineTests
+                    {progress} beginning step step3aa(during:abort:)@PipelineTests
+                    {progress} ending step step3aa(during:abort:)@PipelineTests
+                    {deadly} cannot proceess anything any more
                     {progress} skipping in an aborted environment step step3ab(during:abort:)@PipelineTests
                 {progress} ending step step2a(during:abort:)@PipelineTests
                 {progress} skipping in an aborted environment step step2b(during:abort:)@PipelineTests
