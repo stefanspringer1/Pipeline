7eb1534c50dfb3ac985ef770861751c3d173580c
stefanspringer1 <stefan.springer@icloud.com>
2025-09-19 17:55:24 +0200

    Update AbortionTests.swift

--- Tests/PipelineTests/AbortionTests.swift
+++ Tests/PipelineTests/AbortionTests.swift
@@ -2,7 +2,7 @@ import Testing
 import Pipeline
 import Foundation
 
-@Suite(.serialized) struct FatalErrorTests {
+@Suite(.serialized) struct AbortionTests {
     
     let metadata = MyMetaData1(
         applicationName: "myapp",
@@ -29,8 +29,7 @@ import Foundation
         
         func step2b<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
             execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
-                step3ba(during: execution)
-                step3bb(during: execution)
+                step3b(during: execution)
             }
         }
         
@@ -46,13 +45,7 @@ import Foundation
             }
         }
         
-        func step3ba<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
-            execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
-                // -
-            }
-        }
-        
-        func step3bb<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
+        func step3b<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
             execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
                 // -
             }
@@ -75,6 +68,68 @@ import Foundation
                 {progress} ending step step2a(during:abort:)@PipelineTests
                 {progress} skipping in an aborted environment step step2b(during:abort:)@PipelineTests
             {progress} ending step step1(during:)@PipelineTests
+            """)
+        
+    }
+    
+    @Test func testAbortion() throws {
+        
+        func step1<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>) {
+            execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
+                step2a(during: execution)
+                step2b(during: execution)
+            }
+        }
+        
+        func step2a<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
+            execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
+                step3aa(during: execution)
+                execution.abort(reason: "cannot proceess any further") // !!!! abortion is here !!!!
+                step3ab(during: execution)
+            }
+        }
+        
+        func step2b<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
+            execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
+                step3b(during: execution)
+            }
+        }
+        
+        func step3aa<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
+            execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
+                // -
+            }
+        }
+        
+        func step3ab<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
+            execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
+                // -
+            }
+        }
+        
+        func step3b<MetaData: ExecutionMetaData>(during execution: Execution<MetaData>, abort: Bool = false) {
+            execution.effectuate(checking: StepID(crossModuleFileDesignation: #file, functionSignature: #function)) {
+                // -
+            }
+        }
+        
+        let logger = CollectingLogger()
+        let myExecutionInfoConsumer = ExecutionInfoConsumerForLogger<MyMetaData1>(logger: logger, excutionInfoFormat: ExecutionInfoFormat(withIndentation: true, withType: true))
+        
+        let execution = Execution<MyMetaData1>(metadata: metadata, executionInfoConsumer: myExecutionInfoConsumer)
+        
+        step1(during: execution)
+        
+        #expect(logger.messages.joined(separator: "\n") == """
+            {progress} beginning step step1(during:)@PipelineTests
+                {progress} beginning step step2a(during:abort:)@PipelineTests
+                    {progress} beginning step step3aa(during:abort:)@PipelineTests
+                    {progress} ending step step3aa(during:abort:)@PipelineTests
+                    {progress} aborting execution: cannot proceess any further
+                    {progress} skipping in an aborted environment step step3ab(during:abort:)@PipelineTests
+                {progress} aborted step step2a(during:abort:)@PipelineTests
+                {progress} skipping in an aborted environment step step2b(during:abort:)@PipelineTests
+            {progress} aborted step step1(during:)@PipelineTests
             """)
         
     }
